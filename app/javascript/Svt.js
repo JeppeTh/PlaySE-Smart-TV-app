ddf68da642d3e2b899d8455e4b534ec3478d615c
JeppeTh <JeppeTh@users.noreply.github.com>
2020-12-30 20:57:03 +0100

    Restored stream selection also for finished SVT Live streams.

--- app/javascript/Svt.js
+++ app/javascript/Svt.js
@@ -1128,6 +1128,8 @@ Svt.getPlayUrl = function(url, isLive, streamUrl) {
                            data = JSON.parse(data.responseText);
                        }
 
+                       var isLiveStream = isLive || data.live;
+
                        if (data.video && data.video.subtitleReferences)
                            subtitleReferences = data.video.subtitleReferences;
                        else if (data.video && data.video.subtitles)
@@ -1135,7 +1137,7 @@ Svt.getPlayUrl = function(url, isLive, streamUrl) {
                        else if (data.subtitleReferences)
                            subtitleReferences = data.subtitleReferences;
 
-                       for (var k = 0; !isLive && k < subtitleReferences.length; k++) {
+                       for (var k = 0; !isLiveStream && k < subtitleReferences.length; k++) {
 		           Log('subtitleReferences:' + subtitleReferences[k].url);
                            srtUrl = subtitleReferences[k].url;
                            if (subtitleReferences[k].label &&
@@ -1149,7 +1151,7 @@ Svt.getPlayUrl = function(url, isLive, streamUrl) {
                        else
                            videoReferences = data.videoReferences;
 
-                       Svt.sortStreams(videoReferences, isLive);
+                       Svt.sortStreams(videoReferences, isLiveStream);
                        videoReferences = Svt.stripDuplicatStreams(videoReferences);
                        for (var j = 0; j < videoReferences.length; j++) {
                            alert('format:' + videoReferences[j].format);
