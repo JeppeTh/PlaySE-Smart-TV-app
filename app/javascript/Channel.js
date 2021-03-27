cc86232946a69b0018d871ffb42bd1d928b32511
JeppeTh <JeppeTh@users.noreply.github.com>
2021-03-27 14:26:59 +0100

    Got Viasat (somewhat) working on older Tizen Models.

--- app/javascript/Channel.js
+++ app/javascript/Channel.js
@@ -256,6 +256,10 @@ Channel.tryAltPlayUrl = function(failedUrl, cbComplete) {
     return false;
 };
 
+Channel.addBwFilter = function (bw, bwStream, stream) {
+    return this.impl.addBwFilter(bw, bwStream, stream);
+};
+
 Channel.fetchSubtitles = function(srtUrl, hlsSubs, usedRequestedUrl, cb) {
     Subtitles.init();
     if (typeof srtUrl == 'string' && srtUrl.match(/\.m3u8/)) {
