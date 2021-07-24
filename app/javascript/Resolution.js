571c4e091f0671da6a2e60bee3e2d341f0fbc446
JeppeTh <JeppeTh@users.noreply.github.com>
2021-07-24 19:32:20 +0200

    Fixed SVT Live.

--- app/javascript/Resolution.js
+++ app/javascript/Resolution.js
@@ -106,6 +106,10 @@ Resolution.getCorrectStream = function(videoUrl, srtUrl, extra) {
                    else if (videoUrl.match(/\.ism/)) {
                        videoUrl = videoUrl + '|COMPONENT=WMDRM';
                    }
+                   if (extra.modify_stream) {
+                       var urlPrefix = getUrlPrefix(videoUrl);
+                       extra.stream_content = Channel.modifyStream(urlPrefix,data.responseText);
+                   }
                    Player.setVideoURL(master, videoUrl, srtUrl, extra);
                    extra.cb();
                },
