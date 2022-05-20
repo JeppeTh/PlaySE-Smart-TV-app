65984340d7240f7116208ad4772dfa04a38fcf69
JeppeTh <JeppeTh@users.noreply.github.com>
2022-05-20 21:26:49 +0200

    Some fixes related to prior commit.

--- app/javascript/Player.js
+++ app/javascript/Player.js
@@ -1522,7 +1522,8 @@ Player.getRepeatText = function() {
 
 Player.createPlugin = function(PluginToUse) {
 
-    if (Player.plugin && Player.pluginPlayer!=PluginToUse)
+    var changePlugin = Player.plugin && Player.pluginPlayer!=PluginToUse;
+    if (changePlugin)
         Player.plugin.remove();
 
     Player.pluginPlayer = PluginToUse;
@@ -1531,6 +1532,8 @@ Player.createPlugin = function(PluginToUse) {
     else
         Player.plugin = VideoJsPlayer;
     Player.plugin.create(Player.pluginPlayer == Player.PLUGIN_VIDEOJS_NATIVE);
+    if (changePlugin)
+        Player.setTopOSDText('')
 };
 
 Player.togglePlugin = function() {
