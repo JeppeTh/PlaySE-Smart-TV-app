abe8f01b41eede125b23c410e4089aaa618e0221
JeppeTh <JeppeTh@users.noreply.github.com>
2022-09-26 18:26:10 +0200

    Improve handling/duration of extended live streams.

--- app/javascript/Player.js
+++ app/javascript/Player.js
@@ -1388,8 +1388,8 @@ Player.PlaybackFailed = function(text) {
 };
 
 Player.GetDuration = function() {
-    if (!this.sourceDuration && 
-        Details.fetchedDetails && 
+    if (!this.sourceDuration &&
+        Details.fetchedDetails &&
         Details.fetchedDetails.duration
        )
         Player.setDuration(Details.fetchedDetails.duration);
@@ -1400,12 +1400,17 @@ Player.GetDuration = function() {
             Player.pluginDuration = Player.plugin.getDuration() || 1;
         }
         duration = Player.pluginDuration - Player.durationOffset;
+        if (Player.isLive && !videoData.use_offset) {
+            var liveDuration = Player.plugin.getLiveDuration();
+            if (liveDuration && liveDuration > duration)
+                duration = liveDuration;
+        }
     }
 
     if (this.sourceDuration > duration)
         duration = this.sourceDuration;
 
-    if (ccTime > duration) 
+    if (ccTime > duration)
         duration = ccTime;
 
     return duration;
