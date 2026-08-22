9ce3c8143f9ee132c3f1ad3fc5ba7cbb51c61848
JeppeTh <JeppeTh@users.noreply.github.com>
2026-08-22 14:43:25 +0200

    SVT: Fix startpage

--- app/javascript/Svt.js
+++ app/javascript/Svt.js
@@ -627,7 +627,7 @@ Svt.decodeMain = function(data, extra) {
             data[k].analyticsIdentifiers.listType == 'redaktionell')
             continue;
 
-        if (data[k].id.match(/popul/i)) {
+        if (data[k].id.match(/toplist/i)) {
             PopularIndex = k;
             continue;
         }
